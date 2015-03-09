4cd4daa35e7afe0f9d083634228372e5b56f0912
Danny-mcc <dan@rocketone.co.uk>
2015-03-09 09:12:53 +0000

    Added option for using Moltin Session Store
    Added option for using Moltin Session Store, to fix issue with losing cart items.
    
    https://github.com/moltin/laravel-cart/issues/12

--- src/Moltin/Cart/CartServiceProvider.php
+++ src/Moltin/Cart/CartServiceProvider.php
@@ -25,6 +25,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Config;
 
 use Moltin\Cart\Storage\LaravelSession as SessionStore;
+use Moltin\Cart\Storage\Session as MoltinSessionStore;
 use Moltin\Cart\Storage\LaravelCache as CacheStore;
 use Moltin\Cart\Identifier\Cookie as CookieIdentifier;
 use Moltin\Cart\Identifier\RequestCookie as CookieRequestIdentifier;
@@ -37,6 +38,10 @@ class CartServiceProvider extends ServiceProvider
         switch ($class) {
             case 'session':
                 return new SessionStore;
+            break;
+            
+            case 'moltinSession':
+                return new MoltinSessionStore;
             break;
 
             case 'cache':
